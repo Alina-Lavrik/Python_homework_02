ba61cfcdb57305a831db84e529ab2b2dbc95a042
Alina Lavrik <alina_voro@mail.ru>
2022-11-29 12:14:42 +0300

    Красивый вывод решения

--- Task_002.py
+++ Task_002.py
@@ -7,6 +7,23 @@ system("cls")
 
 import math
 
+''' num = int(input("Введите число: "))
+result = [math.factorial(i) for i in range(1, num + 1)]
+print(f'Произведений чисел равно: {result}') '''
+
+# Решение с подробным выводом
+
+def mult(num: int) -> str:
+    str_mult = '1'
+    for i in range(2, num + 1):
+        if i == num:
+            str_mult += f'*{i}'
+        else:
+            str_mult += f'*{i}'
+    return str_mult
+
 num = int(input("Введите число: "))
 result = [math.factorial(i) for i in range(1, num + 1)]
+example = [mult(i) for i in range(1, num + 1)]
 print(f'Произведений чисел равно: {result}')
+print(f'Пример вычесления: {example}')
